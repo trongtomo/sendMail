3913db93b7a744be8d4f820e01e5eb3dc0a1f642
trongtomo <binguyenquoctrong@gmail.com>
2022-12-16 10:35:33 +0700

    first commit

---
+++ tasks.js
@@ -0,0 +1,65 @@
+var moment = require("moment");
+var nodemailer = require("nodemailer");
+
+const http = require("http"); // or 'https' for https:// URLs
+const fs = require("fs");
+const { log } = require("console");
+
+const baseUrl = "http://192.168.1.32:92";
+const start_date = moment().subtract(2, "day").format("DD-MM-YYYY");
+const end_date = moment().subtract(1, "day").format("DD-MM-YYYY");
+const file = fs.createWriteStream(`${start_date}_${end_date}.zip`);
+const request = http.get(
+  `${baseUrl}/Report/Daily?start=${start_date}&end=${end_date}`,
+  function (response) {
+    response.pipe(file);
+    // after download completed close filestream
+    file.on("finish", () => {
+      file.close();
+      console.log("Download Completed");
+      sendmail();
+    });
+  }
+);
+
+//2 auth and create app
+let mailTransporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "binguyenquoctrong@gmail.com",
+    pass: "pfdkroflbntfqvcx",
+  },
+});
+let mailDetails = {
+  from: "binguyenquoctrong@gmail.com",
+  to: "thanhnguyet@phumyphuong.vn",
+  cc: [
+    "quangvinh@phumyphuong.vn,",
+    '"nquangky@gmail.com" <nquangky@gmail.com>',
+    "vinhloc@phumyphuong.vn",
+    "nguyenlan@phumyphuong.vn",
+    '"lientam.pham@gmail.com" <lientam.pham@gmail.com>',
+    '"linhdtm2711@gmail.com" <linhdtm2711@gmail.com>',
+    '"minhle1508@gmail.com" <minhle1508@gmail.com>',
+    '"thanhnguyet03@gmail.com" <thanhnguyet03@gmail.com>',
+    "developer@phumyphuong.vn",
+  ],
+  subject: `[MOPA] Dữ liệu Mopa & Suppliers từ ngày ${start_date} đến ${end_date}`,
+  html: `<p>Hi Chị Nguyệt,</p></br><p>Gửi chị dữ liệu cho Mopa, các suppliers (DMS, Phát) từ ngày ${start_date} đến ${end_date}</p></br><p>Thanks & Best Regards,</p></br><p></p>Trong Nguyen.</p>`,
+  attachments: [
+    {
+      filename: `${file.path}`,
+      path: __dirname + `/${file.path}`,
+      cid: "uniq-mailtrap.png",
+    },
+  ],
+};
+function sendmail() {
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log("Error Occurs", err.message);
+    } else {
+      console.log("Email sent successfully" + data.response);
+    }
+  });
+}
